08151e4c0f995432af3c11cfb0a1bac693ef01bb
livia <livianascimento814@gmail.com>
2024-04-23 17:09:51 -0300

    props

--- 2_AVANCANDO/avancando/src/App.jsx
+++ 2_AVANCANDO/avancando/src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from 'react';
 import './App.css'
 
 import Night from './assets/night.jpg';
 import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUSerName from './components/ShowUserName';
+import CarDetails from './components/CarDetails';
 
 function App() {
+  const [userName] = useState("Ana");
 
   return (
+    
     <div className="App">
       <h1>Avançando em React</h1>
       {/*Imagem em public*/}
@@ -21,6 +26,13 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      {/* props */}
+      <ShowUSerName name = {userName}/>
+      <h1>Props destructuring</h1>
+      {/* Destructuring */}
+      <CarDetails brand="Renault" km={10000} color="Dourado" novo={false} />
+      <CarDetails brand="Ford" km={0} color="Branco" novo={true} />
+      <CarDetails brand="VW" km={100000} color="Branco" novo={false}/>
     </div>
   )
 }
