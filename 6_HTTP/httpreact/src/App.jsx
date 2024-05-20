a8b282f63f1e095117515b36e6e5c59cf79e7097
livia <livianascimento814@gmail.com>
2024-05-19 22:54:34 -0300

    resgatando dados da api

--- 6_HTTP/httpreact/src/App.jsx
+++ 6_HTTP/httpreact/src/App.jsx
@@ -1,10 +1,34 @@
 import './App.css'
 
+import { useState, useEffect } from 'react'
+
+const url = "http://localhost:3000/products";
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+// 1 - Resgatando dados 
+  useEffect(() => {
+    async function fetchData(){
+      const res = await fetch(url);
+
+      // é utilizado json() para ler o corpo da resposta e convertê-lo de json para um objeto 
+      const data = await res.json();
+  
+      setProducts(data)
+    }
+   fetchData();
+    }, []);
+
 
   return (
     <div className='App'>
       <h1>Lista de Produtos</h1>
+     <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.name} - R$: {product.price}</li>
+      ))}
+     </ul>
     </div>
   )
 }
